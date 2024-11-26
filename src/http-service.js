468ad4e11608e0c96e296e7ab76f49ae04fc0de0
Dimas Bagas Saputro <dimazzbagazz@yahoo.co.id>
2024-11-26 11:36:28 +0700

    Change root to api folder

--- src/http-service.js
+++ src/http-service.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const httpService = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://port-api-liard.vercel.app/',
   headers: {
     'Content-Type': 'application/json',
   },
