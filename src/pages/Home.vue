3438353387325183a381d9720bb13497bc5c0bca
Dimas Bagas Saputro <dimazzbagazz@yahoo.co.id>
2024-11-27 18:27:27 +0700

    Change font

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -25,12 +25,15 @@ const skills = [
   {
     name: 'Tailwind',
     icon: tailwind,
-  }
+  },
 ]
 </script>
 
 <template>
-  <section id="home" class="py-5 flex flex-col min-h-96 justify-center gap-5 max-w-full mx-auto my-10 text-white">
+  <section
+    id="home"
+    class="py-5 flex flex-col min-h-96 justify-center gap-5 max-w-full mx-auto my-10 text-white"
+  >
     <div class="max-w-screen-md mx-auto">
       <div class="relative inline-block px-16 py-4 text-center mx-auto w-full my-5">
         <!-- Left Border -->
@@ -46,40 +49,46 @@ const skills = [
         </div>
 
         <v-typical
-          class="blink text-center text-2xl md:text-5xl font-bold rounded-xl py-5 neue"
+          class="blink text-center text-2xl md:text-3xl font-bold rounded-xl py-5 raleway"
           :steps="[
-          'I am',
-          1000,
-          'I am Dimas Bagas Saputro ',
-          3000,
-          `I am a Frontend Developer 👨‍💻`,
-          1000,
-        ]"
+            'I am',
+            1000,
+            'I am Dimas Bagas Saputro ',
+            3000,
+            `I am a Frontend Developer 👨‍💻`,
+            1000,
+          ]"
           :loop="Infinity"
           :wrapper="'h2'"
         ></v-typical>
       </div>
-      <h5 class="text-center neue text-xl">
+      <h5 class="text-center raleway text-xl">
         I have a passion for creating
-        <span class="font-bold text-green-600 hover:text-3xl transition-all duration-300">beautiful and functional</span> websites.
-        <br> I am also
-        interested in learning new
-        <span class="font-bold text-green-600 hover:text-3xl transition-all duration-300">technologies and frameworks</span>.
-        <br> I am always
-        looking for new challenges and opportunities to
-        <span class="font-bold text-green-600 hover:text-3xl transition-all duration-300">learn and grow</span>.
+        <span class="font-bold text-green-600 hover:text-2xl transition-all duration-300"
+          >beautiful and functional</span
+        >
+        websites.
+        <br />
+        I am also interested in learning new
+        <span class="font-bold text-green-600 hover:text-2xl transition-all duration-300"
+          >technologies and frameworks</span
+        >. <br />
+        I am always looking for new challenges and opportunities to
+        <span class="font-bold text-green-600 hover:text-2xl transition-all duration-300"
+          >learn and grow</span
+        >.
       </h5>
     </div>
 
     <div class="max-w-screen-md mx-auto mt-6">
-      <h1 class="text-2xl font-semibold text-center mb-4">Created with Love &#10084;</h1>
+      <h1 class="text-2xl font-semibold raleway text-center mb-4">Created with Love <span class="text-red-600">&#10084;</span> </h1>
       <div class="relative inline-flex items-center gap-8 px-12">
         <!-- Left bracket -->
-<!--        <div class="absolute left-0 top-0 bottom-0 flex flex-col items-start">-->
-<!--          <div class="w-5 h-[2px] bg-white"></div>-->
-<!--          <div class="w-[2px] h-full bg-white"></div>-->
-<!--          <div class="w-5 h-[2px] bg-white"></div>-->
-<!--        </div>-->
+        <!--        <div class="absolute left-0 top-0 bottom-0 flex flex-col items-start">-->
+        <!--          <div class="w-5 h-[2px] bg-white"></div>-->
+        <!--          <div class="w-[2px] h-full bg-white"></div>-->
+        <!--          <div class="w-5 h-[2px] bg-white"></div>-->
+        <!--        </div>-->
 
         <!-- Programming languages/frameworks -->
         <div class="flex flex-wrap justify-center gap-10">
@@ -93,23 +102,17 @@ const skills = [
         </div>
 
         <!-- Right bracket -->
-<!--        <div class="absolute right-0 top-0 bottom-0 flex flex-col items-end">-->
-<!--          <div class="w-5 h-[2px] bg-white"></div>-->
-<!--          <div class="w-[2px] h-full bg-white"></div>-->
-<!--          <div class="w-5 h-[2px] bg-white"></div>-->
-<!--        </div>-->
+        <!--        <div class="absolute right-0 top-0 bottom-0 flex flex-col items-end">-->
+        <!--          <div class="w-5 h-[2px] bg-white"></div>-->
+        <!--          <div class="w-[2px] h-full bg-white"></div>-->
+        <!--          <div class="w-5 h-[2px] bg-white"></div>-->
+        <!--        </div>-->
       </div>
     </div>
   </section>
 </template>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
-
-.neue {
-  font-family: 'Bebas Neue', cursive;
-}
-
 .blink::after {
   content: '|';
   animation: blink 1s infinite step-start;
@@ -120,6 +123,4 @@ const skills = [
     opacity: 0;
   }
 }
-
-
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,vue,tsx}'],
   darkMode: 'media',
   theme: {
+    fontFamily: {
+    },
     extend: {},
   },
   plugins: [],
