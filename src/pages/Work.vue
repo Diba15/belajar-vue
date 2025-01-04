dfe366d58da1bf5a72ac8d62398261896aab7e1e
Dimas Bagas Saputro <dimazzbagazz@yahoo.co.id>
2025-01-04 19:01:43 +0700

    add paginate to project list

--- src/pages/Work.vue
+++ src/pages/Work.vue
@@ -1,7 +1,7 @@
 <script setup>
 import CustomCard from '@/components/CustomCard.vue'
 import ProjectService from '@/services/model/Project.js'
-import { onMounted, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 
 defineOptions({
   name: 'WorkPage',
@@ -19,21 +19,58 @@ onMounted(async () => {
   }
 })
 
-// const skills = computed(() => {
-//   return new Set(projects.value.flatMap((project) => project.subjects))
-// })
+const currentPage = ref(1)
+const itemsPerPage = ref(3)
+
+const paginatedProjects = computed(() => {
+  const start = (currentPage.value - 1) * itemsPerPage.value
+  const end = start + itemsPerPage.value
+  return projects.value.slice(start, end)
+})
+
+const totalPages = computed(() => {
+  return Math.ceil(projects.value.length / itemsPerPage.value)
+})
+
+const nextPage = () => {
+  if (currentPage.value < totalPages.value) {
+    currentPage.value++
+  }
+}
+
+const prevPage = () => {
+  if (currentPage.value > 1) {
+    currentPage.value--
+  }
+}
 </script>
 
 <template>
   <div
     class="py-5 flex flex-col gap-5 min-h-fit max-w-screen-xl mx-auto px-5 my-5 text-white"
   >
-    <h1 class="font-bold text-3xl">Portfolio</h1>
+    <div class="flex justify-between items-center">
+      <div>
+        <h1 class="font-bold text-3xl">Portfolio</h1>
+        <p class="text-sm text-gray-300">
+          Here are some of my projects that I have worked on.
+        </p>
+      </div>
+      <div class="flex gap-4 justify-end self-end">
+        <button @click="prevPage" :disabled="currentPage === 1">
+          <i class="pi pi-chevron-circle-left"></i>
+        </button>
+        <span> {{ currentPage }}/{{ totalPages }}</span>
+        <button @click="nextPage" :disabled="currentPage === totalPages">
+          <i class="pi pi-chevron-circle-right"></i>
+        </button>
+      </div>
+    </div>
     <hr>
     <div class="flex flex-col gap-5">
-      <div v-if="projects.length > 0" class="flex flex-col gap-5">
+      <div v-if="paginatedProjects.length > 0" class="flex flex-col gap-5">
         <CustomCard
-          v-for="project in projects"
+          v-for="project in paginatedProjects"
           v-bind:key="project.title"
           :title="project.title"
           :description="project.description"
@@ -42,7 +79,7 @@ onMounted(async () => {
           :link="project.link"
           :githubLink="project.githubLink"
         />
-        <p class="text-center text-sm text-gray-300 mb-10">Stay tuned for more updates!</p>
+        <p v-if="currentPage === paginatedProjects.length" class="text-center text-sm text-gray-300 mb-10">Stay tuned for more updates!</p>
       </div>
       <div v-else class="flex items-center justify-center">
         <div class="animate-spin rounded-full h-5 w-5 border-b-2 border-green-600"></div>
